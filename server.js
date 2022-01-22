bd5b9982657b8461c2e74aab95dcf56b5483638d
chengyang9966 <chengyang9966@gmail.com>
2022-01-22 11:04:54 +0800

    ready for deployment

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css; frame-src 'self';"
+  );
+  next();
+});
+
 app.use(express.static(__dirname + "/dist"));
 
 app.all("*", (req, res) => {
